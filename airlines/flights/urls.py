7b821da5368265836b64e7f4f68d147db0d94951
ParthSoni <parthsoni0608@gmail.com>
2021-01-04 01:50:23 -0800

    This is my fifth commit

--- airlines/flights/urls.py
+++ airlines/flights/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("",views.home, name = "home"),
     path("<int:flight_id>",views.flight_det, name="flight_details"),
-    path("book/<int:flight_id>",views.book, name="flight-book")
+    path("book/<int:flight_id>", views.book, name = "flight_book")
 
 ]
--- airlines/flights/views.py
+++ airlines/flights/views.py
@@ -14,15 +14,15 @@ def flight_det(request, flight_id):
         'flight': flight_details,
         'passengers': passenger_detail ,
         'npassengers': passenger.objects.exclude(flight = flight_details).all()
+
     })
 
 def book(request, flight_id):
     if request.method == "POST":
         flight_info = flights.objects.get(pk = flight_id)
-        passenger_info = passenger.objects.get(pk= int(request.POST["passenger_select"]))
+        passenger_info = passenger.objects.get(pk = int(request.POST["passenger-select"]))
         passenger_info.flight.add(flight_info)
-        return HttpResponseRedirect(reverse("flight_details",args = (flight_id,)))
-
+        return HttpResponseRedirect(reverse("flight_details",args=(flight_id,)))
 
 
 
